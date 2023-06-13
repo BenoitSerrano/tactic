2a4420068268ed0017214b2f41c884b7a266f9b2
BenoitSerrano <benoit.serrano10@gmail.com>
2023-06-13 14:02:06 +0200

    initial commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+import Express, { Response } from 'express';
+import cors from 'cors';
+import path from 'path';
+import { config } from './src/config';
+
+const app = Express();
+
+app.use(cors());
+
+app.use(Express.static(path.join(__dirname, '..', 'src', 'client', 'build')));
+
+app.get('/*', (_, res: Response) => {
+    res.sendFile(path.join(__dirname, '..', 'src', 'client', 'build', 'index.html'));
+});
+
+app.listen(config.PORT, async () => {
+    console.log(`Server is running on port ${config.PORT}`);
+});
