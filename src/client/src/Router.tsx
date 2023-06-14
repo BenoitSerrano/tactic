8b0c7669ea287b3725b88db53dd240ff7667d2d6
BenoitSerrano <benoit.serrano10@gmail.com>
2023-06-14 17:33:49 +0200

    delete useless routes

--- src/client/src/Router.tsx
+++ src/client/src/Router.tsx
@@ -1,14 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
 import { Exams } from './pages/Exams';
 import { ExamEdition } from './pages/ExamEdition';
 
 function Router() {
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
             <Route path="/exams" element={<Exams />} />
-            <Route path="/exams/:examId/take" element={<Home />} />
             <Route path="/exams/:examId/edit" element={<ExamEdition />} />
         </Routes>
     );
