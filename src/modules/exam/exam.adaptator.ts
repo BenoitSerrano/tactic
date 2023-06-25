069addd8e865210e04ed4e90337b2b6255a35e68
BenoitSerrano <benoit.serrano10@gmail.com>
2023-06-25 17:29:54 +0100

    add insensitive results

--- src/modules/exam/exam.adaptator.ts
+++ src/modules/exam/exam.adaptator.ts
@@ -4,19 +4,6 @@ const examAdaptator = {
     convertExamWithAttemptsToResults,
 };
 
-// type examWithAttemptsType = {
-//     id: string;
-//     name: string;
-//     questionsChoixMultiple: Array<{ id: number; rightAnswerIndex: number }>;
-//     attempts: Array<{
-//         id: string;
-//         startedAt: string;
-//         endedAt: string | undefined;
-//         student: { id: string; email: string };
-//         qcmAnswers: Array<{ choice: number; id: number; questionChoixMultiple: { id: number } }>;
-//     }>;
-// };
-
 function convertExamWithAttemptsToResults(examWithAttempts: Exam) {
     const examWithResults = examWithAttempts.attempts.map((attempt) => {
         const choices: Record<number, number> = {};
@@ -39,7 +26,10 @@ function convertExamWithAttemptsToResults(examWithAttempts: Exam) {
             0,
         );
         const questionTrouMark = examWithAttempts.questionsTrou.reduce((sum, questionTrou) => {
-            if (questionTrou.rightAnswer === answers[questionTrou.id]) {
+            if (
+                questionTrou.rightAnswer.trim().toLowerCase() ===
+                answers[questionTrou.id].trim().toLowerCase()
+            ) {
                 return sum + 1;
             } else if (
                 questionTrou.acceptableAnswers.some(
