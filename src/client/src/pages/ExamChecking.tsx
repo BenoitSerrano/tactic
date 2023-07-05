4387ceb5034c2a803b6532efcc5307db66417db7
BenoitSerrano <benoit.serrano10@gmail.com>
2023-07-05 12:32:49 +0200

    fix style for exam checking page

--- src/client/src/pages/ExamChecking.tsx
+++ src/client/src/pages/ExamChecking.tsx
@@ -4,7 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 import { api } from '../lib/api';
 import { QuestionChoixMultipleChecking } from '../components/QuestionChoixMultipleChecking';
 import { QuestionTrouChecking } from '../components/QuestionTrouChecking';
-import { Typography } from '@mui/material';
+import { Typography, styled } from '@mui/material';
 import { Page } from '../components/Page';
 
 function ExamChecking() {
@@ -14,30 +14,39 @@ function ExamChecking() {
 
     return !!query.data ? (
         <Page>
-            <Typography variant="h1">{query.data.exam.name}</Typography>
-            {query.data.exam.questionsChoixMultiple.map(
-                (questionChoixMultiple: any, index: number) => (
-                    <QuestionChoixMultipleChecking
-                        key={questionChoixMultiple.id}
+            <MainContainer>
+                <Typography variant="h1">{query.data.exam.name}</Typography>
+                {query.data.exam.questionsChoixMultiple.map(
+                    (questionChoixMultiple: any, index: number) => (
+                        <QuestionChoixMultipleChecking
+                            key={questionChoixMultiple.id}
+                            attemptId={attemptId}
+                            index={index}
+                            questionChoixMultiple={questionChoixMultiple}
+                        />
+                    ),
+                )}
+                <hr />
+                {query.data.exam.questionsTrou.map((questionTrou: any, index: number) => (
+                    <QuestionTrouChecking
+                        key={questionTrou.id}
                         attemptId={attemptId}
                         index={index}
-                        questionChoixMultiple={questionChoixMultiple}
+                        questionTrou={questionTrou}
                     />
-                ),
-            )}
-            <hr />
-            {query.data.exam.questionsTrou.map((questionTrou: any, index: number) => (
-                <QuestionTrouChecking
-                    key={questionTrou.id}
-                    attemptId={attemptId}
-                    index={index}
-                    questionTrou={questionTrou}
-                />
-            ))}
+                ))}
+            </MainContainer>
         </Page>
     ) : (
         <div />
     );
 }
+
+const MainContainer = styled('div')({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    width: '60%',
+});
 
 export { ExamChecking };
