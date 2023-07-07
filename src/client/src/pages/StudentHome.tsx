41370da8d11a4c8be64786cf026e170b85eeca0a
BenoitSerrano <benoit.serrano10@gmail.com>
2023-07-07 15:31:30 +0200

    change text on student home

--- src/client/src/pages/StudentHome.tsx
+++ src/client/src/pages/StudentHome.tsx
@@ -53,10 +53,10 @@ function StudentHome() {
                 <p>
                     <Typography>
                         L'objectif de ce test est de trouver le cours le plus adapté à votre niveau.
-                        Chaque étudiant.e s’engage sur l’honneur à faire le test en 15 minutes
-                        maximum, sans utiliser aucun document et sans faire aucune recherche en
-                        ligne. Si vous ne savez pas, pas de stress : laissez juste la question en
-                        blanc.
+                        Vous vous engagez à n'utiliser aucun document et à ne faire aucune recherche
+                        en ligne. Attention : vous n'aurez pas plus de 15 minutes pour faire ce test
+                        (il y a un chronomètre). Si vous ne savez pas, pas de stress : laissez juste
+                        la question en blanc.
                     </Typography>
                 </p>
                 <p>
