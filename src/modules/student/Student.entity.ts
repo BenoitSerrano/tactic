8207fd0a67c96ec855550f674898fdc4b567a84e
Benoit Serrano <benoit.serrano10@gmail.com>
2023-08-26 03:26:59 -0600

    add uniqueness students

--- src/modules/student/Student.entity.ts
+++ src/modules/student/Student.entity.ts
@@ -1,14 +1,15 @@
-import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn, Unique } from 'typeorm';
 import { StudentInterface } from './student.interface';
 import { AttemptInterface } from '../attempt/attempt.interface';
 import { User } from '../user';
 
 @Entity()
+@Unique('Students are unique by teacher', ['email', 'user'])
 export class Student implements StudentInterface {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
-    @Column({ default: '', unique: true })
+    @Column({ default: '' })
     email: string;
 
     @Column({ nullable: true })
