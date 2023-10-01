993bc54e61aa72b6f78bd03c666084b2dd7a07ad
Benoit Serrano <benoit.serrano10@gmail.com>
2023-10-01 12:42:06 +0200

    handle qcm locally

--- src/client/src/pages/ExamTaking/QuestionChoixMultipleAnswering.tsx
+++ src/client/src/pages/ExamTaking/QuestionChoixMultipleAnswering.tsx
@@ -1,40 +1,23 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import React, { ChangeEvent, useState } from 'react';
-import { api } from '../../lib/api';
+import React, { ChangeEvent } from 'react';
 import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material';
-import { useTimeoutAlert } from './useTimeoutAlert';
 import { questionChoixMultipleType } from './types';
 
 function QuestionChoixMultipleAnswering(props: {
     questionChoixMultiple: questionChoixMultipleType;
     index: number;
     attemptId: string;
+    choice: number;
+    setChoice: (choice: number) => void;
 }) {
-    const { displayTimeoutAlert } = useTimeoutAlert();
-    const queryClient = useQueryClient();
-
-    const upsertQcmAnswerMutation = useMutation({
-        mutationFn: api.createOrUpdateQcmAnswer,
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ['attempts-without-answers'] });
-        },
-        onError: async (error) => {
-            console.warn(error);
-            displayTimeoutAlert();
-        },
-    });
-
-    const [choice, setChoice] = useState<number>(props.questionChoixMultiple.choice);
-
     return (
         <div>
             <FormControl>
                 <FormLabel>
                     {props.index + 1}. {props.questionChoixMultiple.title}
                 </FormLabel>
-                <RadioGroup value={choice} onChange={onChooseQcmAnswer}>
+                <RadioGroup value={props.choice} onChange={onChooseQcmAnswer}>
                     {props.questionChoixMultiple.possibleAnswers.map(
-                        (possibleAnswer: any, index: number) => (
+                        (possibleAnswer, index: number) => (
                             <React.Fragment key={`${props.questionChoixMultiple.id}-${index}`}>
                                 <FormControlLabel
                                     value={index}
@@ -51,12 +34,7 @@ function QuestionChoixMultipleAnswering(props: {
 
     function onChooseQcmAnswer(event: ChangeEvent<HTMLInputElement>) {
         const newChoice = Number(event.target.value);
-        setChoice(newChoice);
-        upsertQcmAnswerMutation.mutate({
-            attemptId: props.attemptId,
-            qcmId: `${props.questionChoixMultiple.id}`,
-            choice: newChoice,
-        });
+        props.setChoice(newChoice);
     }
 }
 
--- src/client/src/pages/ExamTaking/QuestionsAnswering.tsx
+++ src/client/src/pages/ExamTaking/QuestionsAnswering.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { styled } from '@mui/material';
 import { PhraseMelangeeAnswering } from './PhraseMelangeeAnswering';
 import { QuestionChoixMultipleAnswering } from './QuestionChoixMultipleAnswering';
 import { QuestionTrouAnswering } from './QuestionTrouAnswering';
 import { phraseMelangeeType, questionChoixMultipleType, questionTrouType } from './types';
+
+type qcmChoicesType = Record<number, number>;
 
 function QuestionsAnswering(props: {
     attemptId: string;
@@ -11,10 +13,18 @@ function QuestionsAnswering(props: {
     questionsTrou: Array<questionTrouType>;
     phrasesMelangees: Array<phraseMelangeeType>;
 }) {
+    const initialQcmChoices = props.questionsChoixMultiple.reduce((acc, questionChoixMultiple) => {
+        return { ...acc, [questionChoixMultiple.id]: questionChoixMultiple.choice };
+    }, {} as qcmChoicesType);
+
+    const [qcmChoices, setQcmChoices] = useState<qcmChoicesType>(initialQcmChoices);
+
     return (
         <Container>
             {props.questionsChoixMultiple.map((questionChoixMultiple, index: number) => (
                 <QuestionChoixMultipleAnswering
+                    setChoice={buildSetChoice(questionChoixMultiple.id)}
+                    choice={qcmChoices[questionChoixMultiple.id]}
                     key={questionChoixMultiple.id}
                     attemptId={props.attemptId}
                     index={index}
@@ -40,6 +50,10 @@ function QuestionsAnswering(props: {
             ))}
         </Container>
     );
+
+    function buildSetChoice(qcmId: number) {
+        return (choice: number) => setQcmChoices({ ...qcmChoices, [qcmId]: choice });
+    }
 }
 
 const Container = styled('div')({});
