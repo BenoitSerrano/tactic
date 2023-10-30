6453f7f81e46722df36e259919eac579fa990d8b
Benoit Serrano <benoit.serrano10@gmail.com>
2023-10-30 14:19:36 +0100

    delete filter in student adaptator

--- src/modules/student/student.adaptator.ts
+++ src/modules/student/student.adaptator.ts
@@ -10,16 +10,14 @@ function formatStudentsIntoStudentsSummary(
     studentsWithAttempts: Array<Student>,
     exams: Record<Exam['id'], Pick<Exam, 'id' | 'name' | 'duration' | 'extraTime'>>,
 ) {
-    const students = studentsWithAttempts
-        .filter((student) => student.email === 'hehe')
-        .map((studentWithAttempts) => {
-            return {
-                id: studentWithAttempts.id,
-                email: studentWithAttempts.email,
-                createdDate: studentWithAttempts.createdDate,
-                examStatus: computeExamStatus(exams, studentWithAttempts.attempts, new Date()),
-            };
-        });
+    const students = studentsWithAttempts.map((studentWithAttempts) => {
+        return {
+            id: studentWithAttempts.id,
+            email: studentWithAttempts.email,
+            createdDate: studentWithAttempts.createdDate,
+            examStatus: computeExamStatus(exams, studentWithAttempts.attempts, new Date()),
+        };
+    });
     const examNames: Record<Exam['id'], Exam['name']> = {};
     Object.keys(exams).forEach((examId) => {
         examNames[examId] = exams[examId].name;
