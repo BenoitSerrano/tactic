65937317083c3577c6b38d59b8459757f4032a02
Benoit Serrano <benoit.serrano10@gmail.com>
2023-10-31 14:42:18 +0100

    fix design not found

--- src/client/src/pages/NotFound.tsx
+++ src/client/src/pages/NotFound.tsx
@@ -1,12 +1,21 @@
 import { NotLoggedInPage } from '../components/NotLoggedInPage';
-import { Typography } from '@mui/material';
+import { Typography, styled } from '@mui/material';
 
 function NotFound() {
     return (
         <NotLoggedInPage>
-            <Typography variant="h1">La page que vous recherchez n'existe pas.</Typography>
+            <ContentContainer>
+                <Typography variant="h1">La page que vous recherchez n'existe pas.</Typography>
+            </ContentContainer>
         </NotLoggedInPage>
     );
 }
+
+const ContentContainer = styled('div')({
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+});
 
 export { NotFound };
