69b3857c7ab4a267b1dc1106db668cef8f16e756
Benoit Serrano <benoit.serrano10@gmail.com>
2023-11-10 12:48:02 +0100

    create function to compute displayed Answer

---
+++ src/client/src/pages/ExamPages/lib/computeDisplayedAnswer.test.ts
@@ -0,0 +1,330 @@
+import { computeDisplayedAnswer } from './computeDisplayedAnswer';
+
+describe('computeDisplayedAnswer', () => {
+    describe('questionTrou', () => {
+        it('should return the completed title with .... if no answer', () => {
+            const question = {
+                id: 2,
+                title: "Croyez-vous que John....(être) à l'origine de ces lettres anonymes ?",
+                kind: 'questionTrou' as const,
+                possibleAnswers: [],
+                rightAnswers: ['soit'],
+                acceptableAnswers: [],
+                answer: '',
+                mark: 2,
+                points: 2,
+            };
+            const displayedAnswer = computeDisplayedAnswer(question, 'wrong');
+
+            expect(displayedAnswer).toEqual({
+                title: [
+                    {
+                        kind: 'coloredText',
+                        value: "Croyez-vous que John....(être) à l'origine de ces lettres anonymes ?",
+                        status: 'wrong',
+                    },
+                ],
+                answer: undefined,
+            });
+        });
+        it('should return the completed title with color', () => {
+            const question = {
+                id: 2,
+                title: "Croyez-vous que John....(être) à l'origine de ces lettres anonymes ?",
+                kind: 'questionTrou' as const,
+                possibleAnswers: [],
+                rightAnswers: ['soit'],
+                acceptableAnswers: [],
+                answer: 'soit',
+                mark: 2,
+                points: 2,
+            };
+            const displayedAnswer = computeDisplayedAnswer(question, 'right');
+
+            expect(displayedAnswer).toEqual({
+                title: [
+                    { kind: 'text', value: 'Croyez-vous que John' },
+                    { kind: 'coloredText', value: 'soit', status: 'right' },
+                    { kind: 'text', value: "(être) à l'origine de ces lettres anonymes ?" },
+                ],
+                answer: undefined,
+            });
+        });
+    });
+
+    describe('texteATrous', () => {
+        it('should return the title if answers empty', () => {
+            const question = {
+                id: 2,
+                title: 'Portez-vous .... vêtements décontractés pour aller au travail ? Chez .... personne, vous remarquez en premier : .... yeux, .... silhouette, .... sourire?',
+                kind: 'texteATrous' as const,
+                possibleAnswers: [],
+                rightAnswers: ['des', 'une', 'ses', 'sa', 'son'],
+                acceptableAnswers: [],
+                answer: '',
+                mark: 2,
+                points: 2,
+            };
+            const displayedAnswer = computeDisplayedAnswer(question, 'wrong');
+
+            expect(displayedAnswer).toEqual({
+                title: [
+                    {
+                        kind: 'coloredText',
+                        value: 'Portez-vous .... vêtements décontractés pour aller au travail ? Chez .... personne, vous remarquez en premier : .... yeux, .... silhouette, .... sourire?',
+                        status: 'wrong',
+                    },
+                ],
+                answer: undefined,
+            });
+        });
+
+        it('should return the title with color for right answers', () => {
+            const question = {
+                id: 2,
+                title: 'Portez-vous .... vêtements décontractés pour aller au travail ? Chez .... personne, vous remarquez en premier : .... yeux, .... silhouette, .... sourire?',
+                kind: 'texteATrous' as const,
+                possibleAnswers: [],
+                rightAnswers: ['des', 'une', 'ses', 'sa', 'son'],
+                acceptableAnswers: [],
+                answer: 'des une ses sa son',
+                mark: 2,
+                points: 2,
+            };
+            const displayedAnswer = computeDisplayedAnswer(question, 'wrong');
+
+            expect(displayedAnswer).toEqual({
+                title: [
+                    { kind: 'text', value: 'Portez-vous' },
+                    { kind: 'coloredText', value: 'des', status: 'right' },
+                    { kind: 'text', value: 'vêtements décontractés pour aller au travail ? Chez' },
+                    { kind: 'coloredText', value: 'une', status: 'right' },
+                    { kind: 'text', value: 'personne, vous remarquez en premier :' },
+                    { kind: 'coloredText', value: 'ses', status: 'right' },
+                    { kind: 'text', value: 'yeux,' },
+                    { kind: 'coloredText', value: 'sa', status: 'right' },
+                    { kind: 'text', value: 'silhouette,' },
+                    { kind: 'coloredText', value: 'son', status: 'right' },
+                    { kind: 'text', value: 'sourire?' },
+                ],
+                answer: undefined,
+            });
+        });
+
+        it('should return the title with different color for answers', () => {
+            const question = {
+                id: 2,
+                title: 'Portez-vous .... vêtements décontractés pour aller au travail ? Chez .... personne, vous remarquez en premier : .... yeux, .... silhouette, .... sourire?',
+                kind: 'texteATrous' as const,
+                possibleAnswers: [],
+                rightAnswers: ['des', 'une', 'ses', 'sa', 'son'],
+                acceptableAnswers: [],
+                answer: 'des truc ses sa son',
+                mark: 2,
+                points: 2,
+            };
+            const displayedAnswer = computeDisplayedAnswer(question, 'wrong');
+
+            expect(displayedAnswer).toEqual({
+                title: [
+                    { kind: 'text', value: 'Portez-vous' },
+                    { kind: 'coloredText', value: 'des', status: 'right' },
+                    { kind: 'text', value: 'vêtements décontractés pour aller au travail ? Chez' },
+                    { kind: 'coloredText', value: 'truc', status: 'wrong' },
+                    { kind: 'text', value: 'personne, vous remarquez en premier :' },
+                    { kind: 'coloredText', value: 'ses', status: 'right' },
+                    { kind: 'text', value: 'yeux,' },
+                    { kind: 'coloredText', value: 'sa', status: 'right' },
+                    { kind: 'text', value: 'silhouette,' },
+                    { kind: 'coloredText', value: 'son', status: 'right' },
+                    { kind: 'text', value: 'sourire?' },
+                ],
+                answer: undefined,
+            });
+        });
+
+        it('should return the title with several answers not filled', () => {
+            const question = {
+                id: 2,
+                title: 'Portez-vous .... vêtements décontractés pour aller au travail ? Chez .... personne, vous remarquez en premier : .... yeux, .... silhouette, .... sourire?',
+                kind: 'texteATrous' as const,
+                possibleAnswers: [],
+                rightAnswers: ['des', 'une', 'ses', 'sa', 'son'],
+                acceptableAnswers: [],
+                answer: 'des  ses sa son',
+                mark: 2,
+                points: 2,
+            };
+            const displayedAnswer = computeDisplayedAnswer(question, 'wrong');
+
+            expect(displayedAnswer).toEqual({
+                title: [
+                    { kind: 'text', value: 'Portez-vous' },
+                    { kind: 'coloredText', value: 'des', status: 'right' },
+                    { kind: 'text', value: 'vêtements décontractés pour aller au travail ? Chez' },
+                    { kind: 'coloredText', value: '....', status: 'wrong' },
+                    { kind: 'text', value: 'personne, vous remarquez en premier :' },
+                    { kind: 'coloredText', value: 'ses', status: 'right' },
+                    { kind: 'text', value: 'yeux,' },
+                    { kind: 'coloredText', value: 'sa', status: 'right' },
+                    { kind: 'text', value: 'silhouette,' },
+                    { kind: 'coloredText', value: 'son', status: 'right' },
+                    { kind: 'text', value: 'sourire?' },
+                ],
+                answer: undefined,
+            });
+        });
+    });
+
+    describe('texteLibre', () => {
+        it('should return the title and empty answer if not answer provided', () => {
+            const question = {
+                id: 2,
+                title: 'Texte libre ?',
+                kind: 'texteLibre' as const,
+                possibleAnswers: [],
+                rightAnswers: [],
+                acceptableAnswers: [],
+                answer: undefined,
+                mark: 0,
+                points: 2,
+            };
+            const displayedAnswer = computeDisplayedAnswer(question, 'wrong');
+
+            expect(displayedAnswer).toEqual({
+                title: [{ kind: 'text', value: 'Texte libre ?' }],
+                answer: [{ kind: 'coloredText', value: '', status: 'wrong' }],
+            });
+        });
+
+        it('should return the title and colored answer if answer provided', () => {
+            const question = {
+                id: 2,
+                title: 'Texte libre ?',
+                kind: 'texteLibre' as const,
+                possibleAnswers: [],
+                rightAnswers: [],
+                acceptableAnswers: [],
+                answer: "Youpi j'ai une assez bonne note",
+                mark: 1,
+                points: 2,
+            };
+            const displayedAnswer = computeDisplayedAnswer(question, 'acceptable');
+
+            expect(displayedAnswer).toEqual({
+                title: [{ kind: 'text', value: 'Texte libre ?' }],
+                answer: [
+                    {
+                        kind: 'coloredText',
+                        value: "Youpi j'ai une assez bonne note",
+                        status: 'acceptable',
+                    },
+                ],
+            });
+        });
+    });
+    describe('phraseMelangee', () => {
+        it('should return the title and colored answer if no answer provided', () => {
+            const question = {
+                id: 2,
+                title: 'est la vie belle',
+                kind: 'phraseMelangee' as const,
+                possibleAnswers: [],
+                rightAnswers: ['la vie est belle'],
+                acceptableAnswers: [],
+                answer: '',
+                mark: 0,
+                points: 2,
+            };
+            const displayedAnswer = computeDisplayedAnswer(question, 'wrong');
+
+            expect(displayedAnswer).toEqual({
+                title: [{ kind: 'text', value: 'est la vie belle' }],
+                answer: [
+                    {
+                        kind: 'coloredText',
+                        value: '',
+                        status: 'wrong',
+                    },
+                ],
+            });
+        });
+        it('should return the title and colored answer if answer provided', () => {
+            const question = {
+                id: 2,
+                title: 'est la vie belle',
+                kind: 'phraseMelangee' as const,
+                possibleAnswers: [],
+                rightAnswers: ['la vie est belle'],
+                acceptableAnswers: [],
+                answer: 'la vie est belle',
+                mark: 2,
+                points: 2,
+            };
+            const displayedAnswer = computeDisplayedAnswer(question, 'right');
+
+            expect(displayedAnswer).toEqual({
+                title: [{ kind: 'text', value: 'est la vie belle' }],
+                answer: [
+                    {
+                        kind: 'coloredText',
+                        value: 'la vie est belle',
+                        status: 'right',
+                    },
+                ],
+            });
+        });
+    });
+    describe('qcm', () => {
+        it('should return the title and colored answer if no answer provided', () => {
+            const question = {
+                id: 2,
+                title: 'la couleur de mes yeux ?',
+                kind: 'qcm' as const,
+                possibleAnswers: ['rouge', 'bleu', 'vert', 'noir'],
+                rightAnswers: ['bleu'],
+                acceptableAnswers: [],
+                answer: undefined,
+                mark: 0,
+                points: 2,
+            };
+            const displayedAnswer = computeDisplayedAnswer(question, 'wrong');
+
+            expect(displayedAnswer).toEqual({
+                title: [{ kind: 'text', value: 'la couleur de mes yeux ?' }],
+                answer: [
+                    {
+                        kind: 'coloredText',
+                        value: '',
+                        status: 'wrong',
+                    },
+                ],
+            });
+        });
+        it('should return the title and colored answer if answer provided', () => {
+            const question = {
+                id: 2,
+                title: 'la couleur de mes yeux ?',
+                kind: 'qcm' as const,
+                possibleAnswers: ['rouge', 'bleu', 'vert', 'noir'],
+                rightAnswers: ['bleu'],
+                acceptableAnswers: [],
+                answer: '1',
+                mark: 2,
+                points: 2,
+            };
+            const displayedAnswer = computeDisplayedAnswer(question, 'right');
+
+            expect(displayedAnswer).toEqual({
+                title: [{ kind: 'text', value: 'la couleur de mes yeux ?' }],
+                answer: [
+                    {
+                        kind: 'coloredText',
+                        value: 'bleu',
+                        status: 'right',
+                    },
+                ],
+            });
+        });
+    });
+});
--- src/client/src/pages/ExamPages/lib/computeDisplayedAnswer.ts
+++ src/client/src/pages/ExamPages/lib/computeDisplayedAnswer.ts
@@ -1,14 +1,110 @@
-import { questionType } from '../types';
+import { QUESTION_TROU_REGEX, TEXTE_A_TROU_REGEX } from '../../../constants';
+import { answerStatusType, questionType } from '../types';
 
-function computeDisplayedAnswer(question: questionType) {
-    if (question.answer !== undefined && question.answer !== '') {
-        if (question.kind === 'qcm' && question.possibleAnswers !== null) {
-            return question.possibleAnswers[Number(question.answer)];
-        } else {
-            return question.answer;
-        }
+type chunkType =
+    | { kind: 'text'; value: string }
+    | { kind: 'coloredText'; value: string; status: answerStatusType };
+type displayedAnswerType = { title: Array<chunkType>; answer: Array<chunkType> | undefined };
+
+function computeDisplayedAnswer(
+    question: questionType,
+    answerStatus: answerStatusType,
+): displayedAnswerType {
+    switch (question.kind) {
+        case 'questionTrou':
+            const QTRegexMatch = question.title.match(QUESTION_TROU_REGEX);
+            if (!QTRegexMatch) {
+                return { title: [{ kind: 'text', value: question.title }], answer: undefined };
+            }
+            if (!question.answer) {
+                return {
+                    title: [
+                        {
+                            kind: 'coloredText',
+                            value: question.title,
+                            status: 'wrong',
+                        },
+                    ],
+                    answer: undefined,
+                };
+            }
+            const [_, beforeText, afterText] = QTRegexMatch;
+            return {
+                title: [
+                    { kind: 'text', value: beforeText },
+                    { kind: 'coloredText', value: question.answer || '....', status: answerStatus },
+                    { kind: 'text', value: afterText },
+                ],
+                answer: undefined,
+            };
+        case 'texteATrous':
+            const tATRegexMatch = question.title.matchAll(TEXTE_A_TROU_REGEX);
+            let value = tATRegexMatch.next();
+            if (!!value.done) {
+                return { title: [{ kind: 'text', value: question.title }], answer: undefined };
+            }
+            if (!question.answer) {
+                return {
+                    title: [{ kind: 'coloredText', value: question.title, status: 'wrong' }],
+                    answer: undefined,
+                };
+            }
+            const answers = question.answer.split(' ');
+            const title: Array<chunkType> = [];
+            let lastIndexFound = 0;
+            let answerIndex = 0;
+            while (!value.done) {
+                title.push({
+                    kind: 'text',
+                    value: question.title.slice(lastIndexFound, value.value.index).trim(),
+                });
+
+                title.push({
+                    kind: 'coloredText',
+                    value: answers[answerIndex] || '....',
+                    status:
+                        answers[answerIndex] === question.rightAnswers[answerIndex]
+                            ? 'right'
+                            : 'wrong',
+                });
+                lastIndexFound = (value.value.index || 0) + 4;
+                value = tATRegexMatch.next();
+                answerIndex++;
+            }
+            if (lastIndexFound < question.title.length - 1) {
+                title.push({ kind: 'text', value: question.title.slice(lastIndexFound).trim() });
+            }
+            return { title, answer: undefined };
+        case 'texteLibre':
+            return {
+                title: [{ kind: 'text', value: question.title }],
+                answer: [
+                    { kind: 'coloredText', value: question.answer || '', status: answerStatus },
+                ],
+            };
+        case 'phraseMelangee':
+            return {
+                title: [{ kind: 'text', value: question.title }],
+                answer: [
+                    { kind: 'coloredText', value: question.answer || '', status: answerStatus },
+                ],
+            };
+        case 'qcm':
+            return {
+                title: [{ kind: 'text', value: question.title }],
+                answer: [
+                    {
+                        kind: 'coloredText',
+                        value: question.answer
+                            ? question.possibleAnswers[Number(question.answer)]
+                            : '',
+                        status: answerStatus,
+                    },
+                ],
+            };
     }
-    return '';
+
+    return { title: [], answer: undefined };
 }
 
 export { computeDisplayedAnswer };
