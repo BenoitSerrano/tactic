42ef49339b8b9e459134cdc1acbb4af75b4105be
Benoit Serrano <benoit.serrano10@gmail.com>
2023-11-25 23:44:07 +0100

    display progress bar for attempts count

--- src/client/src/pages/ExamPages/ExamChecking/AttemptsCount.tsx
+++ src/client/src/pages/ExamPages/ExamChecking/AttemptsCount.tsx
@@ -1,26 +1,39 @@
-import { Typography, styled } from '@mui/material';
+import { LinearProgress, Typography, styled } from '@mui/material';
 import { attemptsCountByAttemptStatusApiType } from './types';
 import { HEADER_HEIGHT } from '../../../constants';
 
 function AttemptsCount(props: {
     attemptsCountByCorrectedStatus: attemptsCountByAttemptStatusApiType;
 }) {
+    const totalAttemptsCount =
+        props.attemptsCountByCorrectedStatus.corrected +
+        props.attemptsCountByCorrectedStatus.notCorrected;
+    const correctedAttemptsCount = props.attemptsCountByCorrectedStatus.corrected;
+    const progress =
+        totalAttemptsCount > 0 ? (correctedAttemptsCount / totalAttemptsCount) * 100 : 0;
     return (
         <Container>
-            <Typography variant="h4">
-                Copies corrigées : {props.attemptsCountByCorrectedStatus.corrected} /{' '}
-                {props.attemptsCountByCorrectedStatus.corrected +
-                    props.attemptsCountByCorrectedStatus.notCorrected}
-            </Typography>
+            <AttemptsCountText variant="h4">
+                Copies corrigées : {correctedAttemptsCount} / {totalAttemptsCount}
+            </AttemptsCountText>
+            <LinearProgress variant="determinate" value={progress} />
         </Container>
     );
 }
 
 const Container = styled('div')(({ theme }) => ({
     position: 'fixed',
+    borderColor: theme.palette.divider,
+    borderWidth: '1px',
+    padding: theme.spacing(1),
+    borderStyle: 'solid',
+    borderRadius: '10px',
+    backgroundColor: theme.palette.common.white,
     right: theme.spacing(2),
     top: HEADER_HEIGHT,
-    marginTop: theme.spacing(3),
+    marginTop: theme.spacing(2),
 }));
+
+const AttemptsCountText = styled(Typography)(({ theme }) => ({ marginBottom: theme.spacing(1) }));
 
 export { AttemptsCount };
