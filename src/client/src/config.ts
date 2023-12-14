e73c4a7d4b399ed0a2bedc591438eb6db2016231
Benoit Serrano <benoit.serrano10@gmail.com>
2023-12-15 00:02:13 +0100

    add sentry

--- src/client/src/config.ts
+++ src/client/src/config.ts
@@ -2,6 +2,7 @@ const config = {
     API_URL: process.env.REACT_APP_API_URL || '',
     HOST_URL: process.env.REACT_APP_HOST_URL || '',
     PORTIVE_AUTH_TOKEN: process.env.REACT_APP_PORTIVE_AUTH_TOKEN || '',
+    SENTRY_DSN: process.env.REACT_APP_SENTRY_DSN || '',
 };
 
 export { config };
