237f51295b2e348fa059d6f6b0c8865f10108e08
Benoit Serrano <benoit.serrano10@gmail.com>
2023-12-23 21:53:51 -0500

    re-put logo

--- src/client/src/components/Logo.tsx
+++ src/client/src/components/Logo.tsx
@@ -1,7 +1,7 @@
 const HEIGHT = 35;
 
 function Logo() {
-    return <img src="full_logo.png" alt="Logo Tactic" height={HEIGHT} />;
+    return <img src="https://tactic-app.fr/full_logo.png" alt="Logo Tactic" height={HEIGHT} />;
 }
 
 export { Logo };
