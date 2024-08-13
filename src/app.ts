357c539939fb33d324a916d5690089b0c98b00a6
Benoit Serrano <benoit.serrano10@gmail.com>
2024-08-13 14:50:47 +0200

    add helmet and csp

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import Express, { Response } from 'express';
+import helmet from 'helmet';
 import cors from 'cors';
 import path from 'path';
 import bodyParser from 'body-parser';
@@ -19,11 +20,54 @@ async function runApp() {
 
     app.use(Express.static(path.join(__dirname, '..', '..', 'src', 'client', 'build')));
 
-    app.get('/*', (_, res: Response) => {
-        res.sendFile(path.join(__dirname, '..', '..', 'src', 'client', 'build', 'index.html'));
-    });
+    app.get(
+        '/*',
+        helmet({ contentSecurityPolicy: { directives: specificCspDirectives } }),
+        (_, res: Response) => {
+            res.sendFile(path.join(__dirname, '..', '..', 'src', 'client', 'build', 'index.html'));
+        },
+    );
 
     app.listen(config.PORT, async () => {
         logger.info(`Server is running on port ${config.PORT}`);
     });
 }
+
+const specificCspDirectives = {
+    'img-src': [
+        "'self'",
+        'data:',
+        'tactic-app.fr',
+        'files.portive.com',
+        'client.crisp.chat',
+        'image.crisp.chat',
+        'storage.crisp.chat',
+    ],
+    'font-src': ["'self'", 'client.crisp.chat'],
+    'media-src': ["'self'", 'client.crisp.chat'],
+    'style-src': [
+        "'self'",
+        'client.crisp.chat',
+        "'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='",
+        "'sha256-qGZj8fy4Etp9XlooL/Ilz+mx0y4V+Uu+F7tY38W+15g='",
+        "'sha256-MKASWYfd3dGFQes9nQT5XnslE3xYlnUb4cHpxhk4fag='",
+    ],
+    'frame-src': ["'self'", 'game.crisp.chat'],
+    'script-src': [
+        "'self'",
+        'settings.crisp.chat',
+        "'sha256-RtdC0WqE+hX0MgZZk4QgMbkV1woYKbsuKQKKnWxsudI='",
+    ],
+    'script-src-elem': [
+        "'self'",
+        'client.crisp.chat',
+        "'sha256-RtdC0WqE+hX0MgZZk4QgMbkV1woYKbsuKQKKnWxsudI='",
+    ],
+    'connect-src': [
+        "'self'",
+        'wss://client.relay.crisp.chat',
+        'client.crisp.chat',
+        'storage.crisp.chat',
+        'wss://stream.relay.crisp.chat',
+    ],
+};
