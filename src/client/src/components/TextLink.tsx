3a924f944712c2618599804483f71e9a2eeb4eab
Benoit Serrano <benoit.serrano10@gmail.com>
2024-08-20 14:27:44 +0200

    improve design links

---
+++ src/client/src/components/TextLink.tsx
@@ -0,0 +1,31 @@
+import { styled } from '@mui/material';
+import { Link as ReactRouterLink } from 'react-router-dom';
+
+function TextLink(props: {
+    children: React.ReactNode | string;
+    to: string;
+    opensNewTab?: boolean;
+}) {
+    const target = props.opensNewTab ? '_blank' : undefined;
+
+    return (
+        <StyledLink target={target} to={props.to}>
+            {props.children}
+        </StyledLink>
+    );
+}
+
+export { TextLink };
+
+const StyledLink = styled(ReactRouterLink)(({ theme }) => ({
+    color: theme.palette.common.black,
+    display: 'block',
+    textDecoration: 'none',
+    padding: theme.spacing(1),
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    borderRadius: '4px',
+    '&:hover': {
+        backgroundColor: theme.palette.primary.light,
+    },
+}));
