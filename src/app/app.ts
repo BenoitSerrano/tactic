af091d0997cba99e92a895a4519014d8969b4ada
Benoit Serrano <benoit.serrano10@gmail.com>
2024-08-20 13:58:15 +0200

    move csp handler to middleware folder

---
+++ src/app/app.ts
@@ -0,0 +1,31 @@
+import Express, { Response } from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import path from 'path';
+import bodyParser from 'body-parser';
+import { config } from '../config';
+import { dataSource } from '../dataSource';
+import { router } from '../router';
+import { logger } from '../lib/logger';
+import { securityResponseHeaderHandler } from './middlewares/securityResponseHeaderHandler';
+
+export { runApp };
+
+async function runApp() {
+    await dataSource.initialize();
+    logger.info(`Data source has been initialized`);
+
+    const app = Express();
+
+    app.use('/api', cors({ origin: config.CLIENT_URL }), bodyParser.json(), router);
+
+    app.use(Express.static(path.join(__dirname, '..', '..', 'src', 'client', 'build')));
+
+    app.get('/*', securityResponseHeaderHandler, (_, res: Response) => {
+        res.sendFile(path.join(__dirname, '..', '..', 'src', 'client', 'build', 'index.html'));
+    });
+
+    app.listen(config.PORT, async () => {
+        logger.info(`Server is running on port ${config.PORT}`);
+    });
+}
