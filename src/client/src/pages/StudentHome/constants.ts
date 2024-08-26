c03860b4757217e03c9cdb4ebc426b45d9aa4a65
Benoit Serrano <benoit.serrano10@gmail.com>
2024-08-26 11:53:42 +0200

    update texts start and beginning

--- src/client/src/pages/StudentHome/constants.ts
+++ src/client/src/pages/StudentHome/constants.ts
@@ -1,5 +1,5 @@
 const examStartText = `
-L'objectif de ce test est de trouver le cours le plus adapté à votre niveau. Chaque étudiant.e s’engage sur l’honneur à faire le test en 15 minutes maximum, sans utiliser aucun document et sans faire aucune recherche en ligne. Si vous ne savez pas, pas de stress : laissez juste la question en blanc.\n
+L'objectif de ce test est de trouver le cours le plus adapté à votre niveau. Chaque étudiant.e s’engage sur l’honneur à le passer sans utiliser de document et sans faire de recherche en ligne. Le test dure 15 minutes. Faites attention : au-delà des 15 minutes, vos réponses ne sont plus enregistrées. Quand vous avez fini le test, vous pouvez cliquer sur "Terminer l'examen". Si vous ne connaissez pas la réponse à une question, pas de stress : laissez juste la question en blanc.\n
 If you've never studied French, please send an email to Hélène Boisson (helene.boisson@ens.psl.eu).
 `;
 
