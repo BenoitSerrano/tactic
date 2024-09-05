c863b67b48e57cac1f69488f323430d873cf4fff
Benoit Serrano <benoit.serrano10@gmail.com>
2024-09-05 19:01:40 +0200

    use default theme for local

--- src/client/src/config.ts
+++ src/client/src/config.ts
@@ -1,4 +1,5 @@
 const config = {
+    NODE_ENV: process.env.NODE_ENV || 'development',
     API_URL: process.env.REACT_APP_API_URL || '',
     HOST_URL: process.env.REACT_APP_HOST_URL || '',
     PORTIVE_AUTH_TOKEN: process.env.REACT_APP_PORTIVE_AUTH_TOKEN || '',
