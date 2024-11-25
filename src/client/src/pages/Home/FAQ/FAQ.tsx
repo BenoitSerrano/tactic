722553eb955c4780afc6a35f9bb48fe97162c07e
Benoit Serrano <benoit.serrano10@gmail.com>
2024-11-25 14:18:30 +0100

    add faq

--- src/client/src/pages/Home/FAQ/FAQ.tsx
+++ src/client/src/pages/Home/FAQ/FAQ.tsx
@@ -1,9 +1,37 @@
-import { styled } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, styled, Typography } from '@mui/material';
+import { FAQ_CONTENT } from './constants';
 
 function FAQ() {
-    return <Container />;
+    return (
+        <Container>
+            <Title variant="h1">Foire Aux Questions</Title>
+
+            {FAQ_CONTENT.map(({ question, answer }, index) => (
+                <AccordionContainer>
+                    <Accordion key={`faq-${index}`} disableGutters>
+                        <AccordionSummary>
+                            <Typography variant="h4">{question}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>{answer}</Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                </AccordionContainer>
+            ))}
+        </Container>
+    );
 }
 
-const Container = styled('div')(({ theme }) => ({}));
+const Container = styled('div')(({ theme }) => ({
+    padding: theme.spacing(5),
+}));
+const Title = styled(Typography)(({ theme }) => ({
+    textAlign: 'center',
+    marginBottom: theme.spacing(2),
+}));
+const AccordionContainer = styled('div')(({ theme }) => ({
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+}));
 
 export { FAQ };
