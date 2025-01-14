c5af50653ca7f092302e9cb6cb6ee8db69a3f4ab
Benoit Serrano <benoit.serrano10@gmail.com>
2025-01-14 20:30:00 +0100

    move items between lists

--- src/client/src/components/AdminSideMenu/EstablishmentListEditionModal.tsx
+++ src/client/src/components/AdminSideMenu/EstablishmentListEditionModal.tsx
@@ -1,16 +1,32 @@
-import { styled, Typography } from '@mui/material';
+import { styled, Tooltip, Typography, IconButton as MuiIconButton } from '@mui/material';
 import GroupsIcon from '@mui/icons-material/Groups';
 import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 import AccountBalanceOutlinedIcon from '@mui/icons-material/AccountBalanceOutlined';
 import { Modal } from '../Modal';
 import { establishmentWithClassesType } from '../../lib/api/establishmentsApi';
-import { IconButton } from '../IconButton';
+import {
+    DragDropContext,
+    Draggable,
+    DragStart,
+    Droppable,
+    DropResult,
+    OnDragEndResponder,
+    OnDragStartResponder,
+} from 'react-beautiful-dnd';
+import { useState } from 'react';
 
 function EstablishmentListEditionModal(props: {
     isOpen: boolean;
     close: () => void;
     establishments: Array<establishmentWithClassesType>;
 }) {
+    const mappedEstablishements = props.establishments.reduce((acc, establishment) => {
+        return { ...acc, [establishment.id]: establishment };
+    }, {} as Record<string, establishmentWithClassesType>);
+    const [currentEstablishments, setCurrentEstablishments] = useState(mappedEstablishements);
+    const [droppableIdCurrentlyDragging, setDroppableIdCurrentlyDragging] = useState<
+        string | undefined
+    >(undefined);
     return (
         <Modal
             size="large"
@@ -18,42 +34,119 @@ function EstablishmentListEditionModal(props: {
             close={props.close}
             isOpen={props.isOpen}
             onConfirm={onConfirm}
+            confirmButtonLabel="Enregistrer les modifications"
         >
-            <ModalContent>
-                {props.establishments.map((establishment) => (
-                    <SideItemListContainer key={establishment.id}>
-                        <SideItemContainer>
-                            <SideItemMainContentContainer>
-                                <SideItemIconContainer>
-                                    <AccountBalanceOutlinedIcon fontSize={'medium'} />
-                                </SideItemIconContainer>
-                                <Typography variant="h5">{establishment.name}</Typography>
-                            </SideItemMainContentContainer>
-                        </SideItemContainer>
+            <DragDropContext onDragEnd={onDragEnd} onDragStart={onDragStart}>
+                <ModalContent>
+                    {props.establishments.map(({ id }) => {
+                        const establishment = currentEstablishments[id];
+                        return (
+                            <Droppable
+                                isDropDisabled={
+                                    droppableIdCurrentlyDragging === `droppable:${establishment.id}`
+                                }
+                                droppableId={`droppable:${establishment.id}`}
+                                key={establishment.id}
+                            >
+                                {(droppableClasseProvided) => (
+                                    <SideItemListContainer
+                                        ref={droppableClasseProvided.innerRef}
+                                        {...droppableClasseProvided.droppableProps}
+                                    >
+                                        <SideItemContainer>
+                                            <SideItemMainContentContainer>
+                                                <SideItemIconContainer>
+                                                    <AccountBalanceOutlinedIcon
+                                                        fontSize={'medium'}
+                                                    />
+                                                </SideItemIconContainer>
+                                                <Typography variant="h5">
+                                                    {establishment.name}
+                                                </Typography>
+                                            </SideItemMainContentContainer>
+                                        </SideItemContainer>
 
-                        {establishment.classes.map((classe) => (
-                            <SideItemListContainer key={classe.id}>
-                                <SideItemContainer>
-                                    <SideItemMainContentContainer>
-                                        <SideItemIconContainer>
-                                            <GroupsIcon fontSize="small" />
-                                        </SideItemIconContainer>
-                                        <Typography variant="h6">{classe.name}</Typography>
-                                    </SideItemMainContentContainer>
-                                    <IconButton
-                                        IconComponent={DragIndicatorIcon}
-                                        title="Déplacer la classe dans un autre établissement"
-                                    />
-                                </SideItemContainer>
-                            </SideItemListContainer>
-                        ))}
-                    </SideItemListContainer>
-                ))}
-            </ModalContent>
+                                        {establishment.classes.map((classe, classeIndex) => (
+                                            <Draggable
+                                                key={'classe:' + classe.id}
+                                                draggableId={'classe:' + classe.id}
+                                                index={classeIndex}
+                                            >
+                                                {(draggableClasseProvided) => (
+                                                    <SideItemListContainer
+                                                        ref={draggableClasseProvided.innerRef}
+                                                        {...draggableClasseProvided.draggableProps}
+                                                    >
+                                                        <SideItemContainer>
+                                                            <SideItemMainContentContainer>
+                                                                <SideItemIconContainer>
+                                                                    <GroupsIcon fontSize="small" />
+                                                                </SideItemIconContainer>
+                                                                <Typography variant="h6">
+                                                                    {classe.name}
+                                                                </Typography>
+                                                            </SideItemMainContentContainer>
+                                                            <Tooltip title="Maintenez le clic enfoncé pour déplacer la classe dans un autre établissement">
+                                                                <MuiIconButton
+                                                                    {...draggableClasseProvided.dragHandleProps}
+                                                                >
+                                                                    <DragIndicatorIcon />
+                                                                </MuiIconButton>
+                                                            </Tooltip>
+                                                        </SideItemContainer>
+                                                    </SideItemListContainer>
+                                                )}
+                                            </Draggable>
+                                        ))}
+                                        {droppableClasseProvided.placeholder}
+                                    </SideItemListContainer>
+                                )}
+                            </Droppable>
+                        );
+                    })}
+                </ModalContent>
+            </DragDropContext>
         </Modal>
     );
 
     function onConfirm() {}
+    function onDragStart(start: DragStart): ReturnType<OnDragStartResponder> {
+        setDroppableIdCurrentlyDragging(start.source.droppableId);
+    }
+
+    function onDragEnd(result: DropResult): ReturnType<OnDragEndResponder> {
+        if (!result.destination) {
+            return;
+        }
+        if (result.destination.droppableId === result.source.droppableId) {
+            return;
+        }
+
+        const sourceEstablishmentId = result.source.droppableId.split(':')[1];
+        const destinationEstablishmentId = result.destination.droppableId.split(':')[1];
+        const sourceClasseId = result.draggableId.split(':')[1];
+        const sourceClasse = currentEstablishments[sourceEstablishmentId].classes.find(
+            (classe) => classe.id === sourceClasseId,
+        );
+        if (!sourceClasse) {
+            return;
+        }
+        const newSourceEstablishment = {
+            ...currentEstablishments[sourceEstablishmentId],
+            classes: currentEstablishments[sourceEstablishmentId].classes.filter(
+                (classe) => classe.id !== sourceClasseId,
+            ),
+        };
+        const newDestinationEstablishment = {
+            ...currentEstablishments[destinationEstablishmentId],
+            classes: [...currentEstablishments[destinationEstablishmentId].classes, sourceClasse],
+        };
+        setCurrentEstablishments({
+            ...currentEstablishments,
+            [sourceEstablishmentId]: newSourceEstablishment,
+            [destinationEstablishmentId]: newDestinationEstablishment,
+        });
+    }
 }
 
 const SideItemListContainer = styled('div')(({ theme }) => ({
@@ -61,6 +154,7 @@ const SideItemListContainer = styled('div')(({ theme }) => ({
     flexDirection: 'column',
     paddingLeft: theme.spacing(2),
 }));
+
 const SideItemContainer = styled('div')(({ theme }) => ({
     display: 'flex',
     paddingTop: theme.spacing(1),
